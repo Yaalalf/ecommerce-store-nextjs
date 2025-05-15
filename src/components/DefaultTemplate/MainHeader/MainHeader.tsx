3d2e867370c16d879dcfb477ad21985e943a32f9
yaalalf <yaalalf98@gmail.com>
2025-05-15 17:52:31 -0400

    feat: add shopping bag icon to MainHeader and update styles for mini state

--- src/components/DefaultTemplate/MainHeader/MainHeader.tsx
+++ src/components/DefaultTemplate/MainHeader/MainHeader.tsx
@@ -6,6 +6,7 @@ import { H } from "@/lib/components/text";
 import VisibilityObserver from "@/lib/components/misc/visibility-observer/visibility-observer";
 import { useState } from "react";
 import { FiPlusCircle } from "react-icons/fi";
+import { FaBagShopping } from "react-icons/fa6";
 
 export default function MainHeader() {
   const [isMiniState, setIsMiniState] = useState(false);
@@ -21,7 +22,8 @@ export default function MainHeader() {
       <header className={`MainHeader ${isMiniState ? "mini" : ""}`}>
         <Column className="Center" align="center" gap="4">
           <H className="Header" type="h1">
-            Catálogo de Olivia Shop
+            Catálogo de Olivia Shop{" "}
+            {isMiniState && <FaBagShopping className="Icon" />}
           </H>
           {isMiniState || (
             <H className="SubHeader" type="h2">
