68f382bc504aa0ca1c02a8e74e0153ba9fca067b
yaalalf <yaalalf98@gmail.com>
2025-06-12 13:21:44 -0400

    refactor: comment out authentication logic in DashboardLayout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,27 +1,27 @@
-import { auth0 } from "@/auth/auth0";
+// import { auth0 } from "@/auth/auth0";
 import Header from "@/features/dashboard/header";
 import Dashboard from "@/lib/components/layout/dashboard";
 import { NotificationProvider } from "@/lib/components/popups/components/notification/notification-context";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth0.getSession();
+  // const session = await auth0.getSession();
 
-  if (!session) {
-    await redirect("auth/login");
-  }
+  // if (!session) {
+  //   await redirect("auth/login");
+  // }
 
   return (
     <>
-      {session && (
-        <NotificationProvider position="top-right">
-          <Dashboard slotHeader={<Header />}>{children}</Dashboard>
-        </NotificationProvider>
-      )}
+      {/* {session && ( */}
+      <NotificationProvider position="top-right">
+        <Dashboard slotHeader={<Header />}>{children}</Dashboard>
+      </NotificationProvider>
+      {/* )} */}
     </>
   );
 }
