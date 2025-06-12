1af62774fa9ea71d969650dec5dd4af787acb8d0
yaalalf <yaalalf98@gmail.com>
2025-06-12 14:27:29 -0400

    test

--- src/app/api/image_loader_strategy/route.ts
+++ src/app/api/image_loader_strategy/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
-export const runtime = "nodejs";
-
+// export const runtime = "nodejs";
+export const dynamic = "force-static";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const imageUrl = searchParams.get("imageUrl");
